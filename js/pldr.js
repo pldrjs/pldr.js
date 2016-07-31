49b44dc621e1a42ad66a0580fdf9b20aef655b7c
HelloWorld017 <deu07115@gmail.com>
2016-07-31 22:10:47 +0900

    API Base

--- js/pldr.js
+++ js/pldr.js
@@ -1,5 +1,36 @@
 var $ = new JavaImporter(java.lang, Packages.cn.nukkit.plugin, Packages.cn.nukkit.event);
+var EventEmitter = require('events');
 var PldrJS = {};
+
+var commandHandlers = [];
+
+class PldrEventEmitter extends EventEmitter{
+
+}
+
+const PldrEvent = new PldrEventEmitter();
+
+var propertySetter = (name) => {
+	return (obj, value) => {
+		if(value !== undefined){
+			obj[name] = value;
+			return;
+		}
+
+		return obj[name];
+	};
+};
+
+PldrJS.command = (handler) => {
+	commandHandlers.push(handler);
+};
+
+PldrJS.commandHook = (sender, command, label, args) => {
+	//TODO
+	commandHandlers.forEach((v) => {
+		v(sender, command, label, args);
+	});
+};
 
 PldrJS.getPlugin = () => {
 	return PldrJSPlugin;
@@ -9,11 +40,41 @@ PldrJS.print = (str) => {
 	PldrJS.getPlugin().getLogger().info(str);
 };
 
-PldrJS.getKnownEvents = () => {
+// ModPE-like Environment
+// Please refer to https://github.com/Connor4898/ModPE-Docs/wiki
+
+//======Block=======
+PldrJS.Block = {};
+
+//======Entity======
+PldrJS.Entity = {};
+PldrJS.Entity.setX = PldrJS.Entity.getX = PldrJS.Entity.x = propertySetter('x');
+PldrJS.Entity.setY = PldrJS.Entity.getY = PldrJS.Entity.y = propertySetter('y');
+PldrJS.Entity.setZ = PldrJS.Entity.getZ = PldrJS.Entity.z = propertySetter('z');
+PldrJS.Entity.getLevel = (entity) => {
+	return entity.getLevel();
+};
+
+PldrJS.Entity.setLevel = (entity, level) => {
+	return entity.setLevel(level);
+};
+
+//======Event=======
+PldrJS.Event = {};
+
+PldrJS.Event.emit = (event) => {
+	//TODO
+};
+
+PldrJS.Event.getEvent = (eventName) => {
+
+};
+
+PldrJS.Event.getKnownEvents = () => {
 	return PldrJSEvents;
 };
 
-PldrJS.on = (eventName, handler, priority) => {
+PldrJS.Event.on = (eventName, handler, priority) => {
 	var EventExecutor = Java.type('cn.nukkit.plugin.EventExecutor');
 	var handleExecutor = Java.extend(EventExecutor, {
 		execute: (listener, event) => {
@@ -35,5 +96,38 @@ PldrJS.on = (eventName, handler, priority) => {
 	var pluginManager = PldrJS.getPlugin().getServer().getPluginManager();
 	pluginManager.registerEvent(event, new handleListener(), handlePriority, new handleExecutor(), PldrJS.getPlugin());
 };
+
+//=======Item=======
+PldrJS.Item = {};
+
+//======Level=======
+PldrJS.Level = {};
+
+//======Player======
+PldrJS.Player = {};
+
+PldrJS.Player.getCarriedItem = (player) => {
+
+};
+
+PldrJS.Player.getLevel = (player) => {
+
+};
+
+PldrJS.Player.getPitch = (player) => {
+
+};
+
+PldrJS.Player.getPlayerEnt = (playerName) => {
+
+};
+
+PldrJS.Player.getPlayers = () => {
+
+};
+
+//======Script======
+PldrJS.Script = {};
+PldrJS.Script.Event = PldrEvent;
 
 module.exports = PldrJS;
